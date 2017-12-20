929e540f699428fbb0be6d5537993ff772d4d976
christophergoot <christophergoot@gmail.com>
2017-12-20 14:04:39 -0800

    + morgan, router

---
+++ mock-data.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+let MOCK_OBSERVATIONS = { "observations": [
+	{
+		id: 11111111,
+		
+	}
+]};
+
+module.exports = {MOCK_OBSERVATIONS};
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+const morgan = require('morgan');
+app.use(morgan('common'));
 app.use(express.static('public'));
+
 app.listen(process.env.PORT || 8080);
 
 
