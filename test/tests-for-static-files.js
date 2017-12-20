b582a617d10a10b19d844db57702e38193b10c03
christophergoot <christophergoot@gmail.com>
2017-12-20 13:06:31 -0800

    + preliminary test ad testing environment

---
+++ test/tests-for-static-files.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mocha = require('mocha');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+// setting temp port for initial testing
+process.env.PORT = 62256;
+const {app} = require('../server');
+
+
+describe('Static Files tests', () => {
+    // before(() => console.log('will soon start server'));
+    // after(() => console.log('will soon close server'));
+// Add one test Add one test that verifies that when you hit up the root url for your client, you get a 200 status code and HTML.
+    it('should serve a valid index.html', () => {
+        return chai.request(app)
+            .get('').then(res => {
+                res.should.be.html;
+                res.should.have.status(200);
+            })
+    })
+})
