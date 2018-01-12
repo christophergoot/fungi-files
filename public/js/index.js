a1569e9449c4437946ba8032b6b1fd2c664c4e3c
christophergoot <christophergoot@gmail.com>
2018-01-12 11:33:46 -0800

    added location method selector'

--- public/js/index.js
+++ public/js/index.js
@@ -69,21 +69,48 @@ const MOCK_OBSERVATIONS = { "observations": [
 
 ]};
 
-function locationOption (opt) {
-	let optClass = `.${opt}`;
-	$('.loc-opt').not(optClass).addClass('hidden');
-	$('button').not(optClass).removeClass('selected');
-	$(optClass).removeClass('hidden');
-	$(`.${opt}-button`).addClass('selected');
+function locationOption (sec) {
+	// all loc-methods
+	const previousDiv = document.querySelector('.loc-method:not(.grayed)');
+	const previousInputs = previousDiv.querySelectorAll('inputs');
+	previousDiv.classList.add('grayed');
+	if (previousInputs) for (let input of previousInputs) input.setAttribute('readonly', 'readonly');
+
+	const targetDiv = document.querySelector('#'+sec);
+	const targetInputs = targetDiv.querySelectorAll("input");
+	if (targetInputs) for (let input of targetInputs) input.removeAttribute('readonly');
+	targetDiv.classList.remove('grayed');
+	// add readonly to inputs
+	// select target div
+	// remove readonly
+	// remove .grayed
 }
 
-// function showNotes(type) {
-// 	$(`.${type}-button`).addClass('selected');
-// 	$(`.${type}-notes`).toggle('.hidden');
+
+
+
+
+
+// function BADlocationOption (sec) {
+// 	const allDivs = document.querySelectorAll("div.loc-method");
+// 	const allInputs = for(let div of allDivs.querySelector('input');
+// 	allDivs.forEach((div) => div.classList.add("grayed"));
+// 	for(let div of allDivs) div.classList.add("grayed");
+// 	const targetDiv = document.querySelector('#'+sec);
+// 	const targetInputs = targetDiv.querySelectorAll("input");
+// 	// document.getElementById('location').getElementByTagName('div').forEach(function(el) { el.classList.add('grayed')});
+// 	// document.getElementById('location').getElementByTagName('div').classList.add('grayed');
+// 	for (let input of allInputs) input.setAttribute("readonly", "readonly");
+// 	for (let input of targetInputs) input.removeAttribute("readonly");
+// 	targetDiv.classList.remove('grayed');
 // }
 
 
-function show(sec) {
+
+function show(event) {
+	// console.log(event);
+	const sec = event.target.attributes.value.value;
+	// console.log(sec);
 	if (['address', 'latlng', 'exif', 'geolocation']
 		.includes(sec)) locationOption(sec);
 	if (['.observations', '.new.observation']
@@ -107,29 +134,55 @@ function getObservations(callback) {
 	setTimeout(function(){ callback(MOCK_OBSERVATIONS)}, 100);
 }
 
-function renderObservation(obs) {
-	console.log('rendering observation');
-	let obsDate = new Date(obs.obsDate);
-	let obsRender = `<div class="obs-list-item">`;
+function viewObservation(id) {
+	document.getElementById('#obs-list').classList.add('hidden');
+	// render and display observation
+}
+
+function reverseGeocode (latlng) {
+	const geocoder = new google.maps.Geocoder;
+	geocoder.geocode({'location': latlng}, function(results, status) {
+		return (results[0].formatted_address);
+	})	
+}
+
+function submitNewObservation (event) {
+	console.log (event);
+}
+
+async function renderObservation(obs) {
+	const obsDate = new Date(obs.obsDate);
+	const latlng = {lat: obs.location.lat, lng: obs.location.lng};
+	const obsAddress = await reverseGeocode(latlng);
+	let obsRender = `<div class="obs-list-item" onclick="viewObservation('${obs.id}')">`;
 	obsRender += `<img class="obs-thumb" src="${obs.photos[0]}">`
-	if (obs.fungi.nickname) obsRender += `<span class="title"><span class="label">nickname: </span>${obs.fungi.nickname}</span>`;
-	if (obs.fungi.commonName) obsRender += `<span class="title"><span class="label">common name: </span>${obs.fungi.commonName}</span>`
-	if (obs.fungi.genus) obsRender += `<span class="fungi"><span class="label">genus: </span>${obs.fungi.genus} <span class="label">species: </span>${obs.fungi.species}</span><span>`
-	if (obs.obsDate) obsRender += `<span class="fungi"><span class="label">observed </span>${obsDate} <span class="label">around </span>${obs.location.lat}${obs.location.lng}</span>`
+	if (obs.fungi.nickname) obsRender += 
+		`<span class="title"><span class="label">nickname: </span>${obs.fungi.nickname}</span>`;
+	if (obs.fungi.commonName) obsRender += 
+		`<span class="title"><span class="label">common name: </span>${obs.fungi.commonName}</span>`
+	if (obs.fungi.genus) obsRender += 
+		`<span class="fungi"><span class="label">genus: </span>${obs.fungi.genus} 
+		<span class="label">species: </span>${obs.fungi.species}</span><span>`
+	if (obs.obsDate) obsRender += 
+		`<span class="fungi"><span class="label">observed </span>${obsDate.toDateString()} 
+		<span class="label">around </span>${obsAddress}</span>`
 	obsRender += `</div>`
 	return obsRender;
 }
 
 function displayObservations(res) {
-	console.log(res.observations);
 	let obsList = "<h2>Observations</h2>";
 	res.observations.forEach((obs) => obsList += renderObservation(obs));
-	$('.obs-list').html(obsList);
+	document.getElementById('obs-list').innerHTML = obsList;
 }
 
 
 function getAndDisplayObservations() {
     getObservations(displayObservations);
+}
+
+function eventListeners () {
+
 }
 
 $( "button" ).click(function( event ) {
